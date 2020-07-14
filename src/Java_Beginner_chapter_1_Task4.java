8ffbf7304a4ed4f8622ef842c60ec62d83bd4f35
Nataliya <progarulit.by@yandex.ru>
2020-07-15 02:37:11 +0300

    Commit

--- src/Java_Beginner_chapter_1_Task4.java
+++ src/Java_Beginner_chapter_1_Task4.java
@@ -1,9 +1,9 @@
 public class Java_Beginner_chapter_1_Task4 {
     public static void main(String[] args) {
         int x = 2675;
-        while(x != 0) {
+        while (x != 0) {
             System.out.print(x % 10);
-            x = (x - x%10)/10;
+            x = (x - x % 10) / 10;
         }
     }
 }
